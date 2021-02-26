7e0837406a59f53071e0a8aab4f2df4383444f54
Ronald Wagner <69792384+hnrwagner@users.noreply.github.com>
2021-02-27 00:02:18 +0100

    Add files via upload

---
+++ Viewport.py
@@ -0,0 +1,35 @@
+session.viewports['Viewport: 1'].viewportAnnotationOptions.setValues(
+    legendFont='-*-times new roman-medium-r-normal-*-*-180-*-*-p-*-*-*')
+session.viewports['Viewport: 1'].viewportAnnotationOptions.setValues(
+    legendNumberFormat=ENGINEERING)
+session.graphicsOptions.setValues(backgroundColor='#FFFFFF', 
+    backgroundBottomColor='#FFFFFF')
+session.viewports['Viewport: 1'].setValues(displayedObject=None)
+session.viewports['Viewport: 1'].viewportAnnotationOptions.setValues(
+    titleFont='-*-verdana-medium-r-normal-*-*-120-*-*-p-*-*-*')
+session.viewports['Viewport: 1'].viewportAnnotationOptions.setValues(
+    titlePosition=(20, 20))
+session.viewports['Viewport: 1'].viewportAnnotationOptions.setValues(
+    titlePosition=(35, 20))
+session.viewports['Viewport: 1'].viewportAnnotationOptions.setValues(
+    titlePosition=(50, 20))
+session.viewports['Viewport: 1'].viewportAnnotationOptions.setValues(
+    titlePosition=(60, 20))
+session.viewports['Viewport: 1'].viewportAnnotationOptions.setValues(
+    stateFont='-*-verdana-medium-r-normal-*-*-100-*-*-p-*-*-*')
+session.viewports['Viewport: 1'].viewportAnnotationOptions.setValues(
+    stateFont='-*-verdana-medium-r-normal-*-*-120-*-*-p-*-*-*')
+session.viewports['Viewport: 1'].viewportAnnotationOptions.setValues(
+    stateFont='-*-times new roman-medium-r-normal-*-*-120-*-*-p-*-*-*')
+session.viewports['Viewport: 1'].viewportAnnotationOptions.setValues(
+    stateFont='-*-times new roman-medium-r-normal-*-*-140-*-*-p-*-*-*')
+session.viewports['Viewport: 1'].viewportAnnotationOptions.setValues(
+    statePosition=(60, 12))
+session.viewports['Viewport: 1'].viewportAnnotationOptions.setValues(
+    titleFont='-*-times new roman-medium-r-normal-*-*-120-*-*-p-*-*-*')
+session.viewports['Viewport: 1'].viewportAnnotationOptions.setValues(
+    titleFont='-*-times new roman-medium-r-normal-*-*-140-*-*-p-*-*-*')
+session.viewports['Viewport: 1'].viewportAnnotationOptions.setValues(
+    triadSize=5)
+session.viewports['Viewport: 1'].viewportAnnotationOptions.setValues(
+    triadSize=7)
